88d2c990572dcddce691abc77e5f5637e37f5b28
diegovbmachado <diegobardo@hotmail.com>
2023-04-05 17:23:12 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const Sequelize = require('sequelize');
 
- //conexao com o banco de dados
+ //conexao com o banco de dadosssss
  const sequelize = new Sequelize('postapp', 'root','24584558',{
     host: "localhost",
     dialect: 'mysql'
